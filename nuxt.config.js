9dd717bd6bc7bfc0678c9f1ea752a9699144d121
Sk8stevieuk <steven.bedford@hotmail.co.uk>
2023-06-23 14:48:21 +0100

    router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   router:{
-    base:"/"
+    base:""
   },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
