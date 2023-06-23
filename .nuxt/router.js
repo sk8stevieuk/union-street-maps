21722e5e4712eb825cbc12d0d9d80361da564783
Sk8stevieuk <steven.bedford@hotmail.co.uk>
2023-06-23 14:44:37 +0100

    router base

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -12,7 +12,7 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: '/',
+  base: '/customprefix/',
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  router:{
+    base:"/"
+  },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   build: {
